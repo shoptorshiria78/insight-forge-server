9ac43cff1921ef4a7bc6d3758ce4683ed924b5a9
unknown <fayhadahmedmonir@gmail.com>
2024-01-25 12:04:53 +0600

    create blog get api

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ const mongoose = require('mongoose');
 
 // router
 const discusRoutes = require('./src/routes/discus')
+const blogRoutes = require('./src/routes/blog')
 
 
 //middleware
 app.use(express.json());
 app.use(cors());
-
-
-
 
 
 const mongoAtlasUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ssnfvav.mongodb.net/${process.env.DB_DB}?retryWrites=true&w=majority`;
@@ -31,9 +29,10 @@ const mongoAtlasUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PAS
     
 
     app.use(discusRoutes)
+    app.use(blogRoutes)
+
       
       app.listen(port, () => {
         console.log(`server is running on port:${port}`)
         
       })
-      
