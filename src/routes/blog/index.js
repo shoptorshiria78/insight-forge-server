225273ae3932daa307d47205897bb80a1a9f0921
unknown <fayhadahmedmonir@gmail.com>
2024-02-06 18:59:09 +0600

    create search route

--- src/routes/blog/index.js
+++ src/routes/blog/index.js
@@ -2,30 +2,55 @@ const express = require('express');
 const BlogData = require('../../models/Blog');
 const router = express.Router();
 
- /* router.get('/allBlog', async (req, res) => {
-    try {
-      const result = await BlogData.find();
-      console.log(result);
-      res.send(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });  */
-
-  /* search category */
-router.get('/allBlog', async (req, res) => {
+ 
+  router.get('/allBlogs', async (req, res) => {
      const category = req.query.search;
      console.log(category)
-    try {
-      const result = await BlogData.find({category: new RegExp(category,'i') });
-      console.log(result);
-      res.send(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
+
+    try{
+       if(category){
+        const searchResult = await BlogData.find({category: new RegExp(category,'i') });
+        console.log(searchResult)
+        res.send(searchResult)
+        
+       }else{
+        const result = await BlogData.find()
+        console.log(result)
+        res.send(result)
+
+       }
+
+    } catch(error){
+      console.error('Error get blog data')
     }
-  });
+    
+})
+
+router.get('/blog/:id', async(req,res)=>{
+  const blogId ={_id:req.params.id};
+  console.log(blogId)
+  try{
+    const result = await BlogData.findOne(blogId);
+    console.log(result)
+        res.send(result)
+  }catch(error) {
+    // Handle any errors that occurred during the save operation
+    console.error('Error id blog data:', error.message);
+  }
+
+
+} )
+
+
+router.get('/recentPost', async(req,res)=>{
+  const result = await BlogData.find();
+  const recent= result.slice(2,5)
+        console.log(recent)
+        res.send(recent)
+
+
+})
+
 
  router.post('/blog', async (req, res) => {
     try{
@@ -41,7 +66,7 @@ router.get('/allBlog', async (req, res) => {
     
 })  
 
- /* router.post('/allBlog', async (req, res) => {
+  router.post('/allBlog', async (req, res) => {
     try{
     const instance =req.body;
     const savedInstance = await BlogData.insertMany(instance);
@@ -53,8 +78,9 @@ router.get('/allBlog', async (req, res) => {
             console.error('Error saving blog data:', error.message);
           }
     
-})  */
+})  
 
 
 module.exports = router
+
 
